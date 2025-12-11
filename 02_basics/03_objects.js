f30292b8279ebf947aaed51b0dd2815d78a1a26d
Farhan Ahmed Khan <farhannnkhan770@gmail.com>
2025-12-11 07:50:15 +0000

    objects

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -1,23 +1,30 @@
-// 2. constructor
-    // singlton
-// Object.create()
-
-const { jsx } = require("react/jsx-runtime");
-
-
-// 1. literal
+// 1. literal     Jab tum {} laga kar seedha object bana do
 let obj = {a: 1, b: 2,c: 3};
-// 2 ways to call the object
+// TWO WAYS TO CALL THE OBJECT
 // console.log(obj.c)
-// console.log(obj["c"])           // better one
+// console.log(obj["c"])                                // better one
 
-const mySym = Symbol("key1");           // symbol ko declare kiye
 
+const mySym = Symbol("key1");                           // symbol ko declare kiye
 let info = {
     "full name": "Farhan Khan", 
-    [sym1]: "abw2$%",                  // key me add kr diye    (imp)
+    [mySym]: "abw2$%",                                  // key me add kr diye    (imp) ***
     age: 19,
+    isFalse: true,
     knowledge: ["c", "java", "html", "CSS", "js", "React", "sprint-boot"]
 }
-console.log(info["full name"])  // . wale method se nhi kar sakte isko isiliye better one
-console.log(info)
+// Accessing VALUES and KEYS.
+// console.log(info["full name"])                          // . wale method se nhi kar sakte isko isiliye better one
+// console.log(info)
+// CHANGING VALUES
+// Object.freeze(info)                                     // value change nhi hogi 
+info.knowledge = ["c", "java", "html", "CSS", "js", "React", "sprint-boot", "ai", "devOps"]
+// console.log(info)
+
+
+
+
+// SINGLETON ?
+// Pure program me sirf ONE object create hota hai  
+// Chahe jitni baar call karo → same instance milta hai  
+// Example: Ek hi TV remote, sab usko use karte hain  
--- README.md
+++ README.md
@@ -3,4 +3,6 @@ A repository containing JavaScript concepts
 
 Firstly,the repository contains the basics of java-script
 
-It Includes : variables, datatypes, conversions, operators, Strings, Numbers and maths etc
+It Includes basics like : 
+1. variables, datatypes, conversions, operators, Strings, Numbers and maths etc
+2. Arrays, objects
