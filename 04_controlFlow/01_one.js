1ed375757ed8726d12431d3a2398c9bce92da9e7
Farhan Ahmed Khan <farhannnkhan770@gmail.com>
2025-12-17 14:06:24 +0000

    conditions

---
+++ 04_controlFlow/01_one.js
@@ -0,0 +1,20 @@
+// if
+const temperature = 93;
+if(temperature < 50) {
+    // console.log("Less than 50")
+}
+else {
+    // console.log("Temperature is greater than 50")
+}
+
+// if, else if
+const score = 200
+if(score>180) {
+    const power = "powerFly"
+    // console.log(`User power: ${power}`)
+} else if (score) {
+    const power = "fly"
+    // console.log(`User power: ${power}`)
+} else {
+    // console.log("Error detected no power fly")
+}
